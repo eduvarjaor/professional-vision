e32d36c133e6e002f67df540eb2a20eac1e6b9dd
eduvarjaor <eduardovarjaor@gmail.com>
2023-10-01 15:11:43 -0300

    feat(UploadImage): placed an area with a button to upload the photo

--- src/components/UploadImage.tsx
+++ src/components/UploadImage.tsx
@@ -1,7 +1,12 @@
 function UploadImage() {
     return ( 
-        <div className="bg-green-300 h-[60vh] w-[35vw]">
-            Upload
+        <div className="h-[60vh] w-[35vw] rounded-3xl shadow-lg bg-slate-50">
+            <div className="flex flex-col mt-[25vh] items-center">
+                <button className="bg-blue-500 hover:bg-blue-700 text-white py-7 px-8 rounded-full text-2xl w-[20vw] shadow-lg">
+                    Upload Image
+                </button>
+                <span className="text-xl mt-[1.4rem]">or drop a file</span>
+            </div>
         </div>
     );
 }
